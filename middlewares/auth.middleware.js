b83c54e9b8b0da44316be69559ff20d25040b7b9
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-08-18 18:17:52 +0530

    Created a Middleware for checking the Permitted or not

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -85,3 +85,50 @@ exports.authoriseAdmin = async (req, res, next) => {
 		});
 	}
 };
+
+exports.isPermitted = async (req, res, next) => {
+	let token;
+	if (
+		req.cookies.token ||
+		(req.headers.authorization &&
+			req.headers.authorization.startsWith("Bearer"))
+	) {
+		token = req.cookies.token || req.headers.authorization.split(" ")[1];
+	}
+
+	if (!token) {
+		res.status(401).json({
+			success: false,
+			message: "Not authorized to access this route",
+		});
+	}
+
+	try {
+		const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET);
+
+		req.user = await db.user.findUnique({
+			where: {
+				id: decodedJwtPayload.id,
+			},
+			select: {
+				name: true,
+				email: true,
+				role: true,
+			},
+		});
+
+		if (req.user.role === "ADMIN" || req.user.role === "MANAGER") {
+			return next();
+		}
+
+		return res.status(401).json({
+			success: false,
+			message: "Not authorized to access this route",
+		});
+	} catch (error) {
+		res.status(401).json({
+			success: true,
+			message: "Not authorized to access this route",
+		});
+	}
+};
