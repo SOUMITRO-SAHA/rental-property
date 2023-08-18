db1cd8c0a5a6b97b171d43acab0f3c06b0bc5599
Soumitra Saha <soumitrosahaofficial@gmail.com>
2023-08-18 17:51:40 +0530

    ticket controllers and routes created |Proper testing required

--- routes/ticket.routes.js
+++ routes/ticket.routes.js
@@ -1,7 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const ticketController = require("../controller/ticket.controller");
+const { isPermitted } = require("../middlewares/auth.middleware");
 
+// This route will be accessible to all the users & Tenents
 router.post("/create", ticketController.createTicket);
+
+// This should only available to the People with permission
+// Like ADMIN AND MANAGER.
+router.post("/all", isPermitted, ticketController.getAllTickets);
+router.post("/:ticketId", isPermitted, ticketController.getTicketById);
+router.post(
+	"/priority/high",
+	isPermitted,
+	ticketController.getHighPriorityTickets
+);
+router.post("/update", isPermitted, ticketController.updateTicketStatus);
 
 module.exports = router;
